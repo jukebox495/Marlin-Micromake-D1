57bb32419d4f99720cca6c7a51a106ccd4628067
jukebox495 <154055121+jukebox495@users.noreply.github.com>
2024-02-11 15:08:30 +0100

    Removed "Change Media" menu entry
    The option to change the media has no effect when using a SD card. Removing the menu entry makes the menu less cluttered.

--- Marlin/src/lcd/menu/menu_main.cpp
+++ Marlin/src/lcd/menu/menu_main.cpp
@@ -247,7 +247,7 @@ void menu_main() {
       if (card_detected) {
         if (!card_open) {
           #if HAS_SD_DETECT
-            GCODES_ITEM(MSG_CHANGE_MEDIA, F("M21"));        // M21 Change Media
+            //GCODES_ITEM(MSG_CHANGE_MEDIA, F("M21"));        // M21 Change Media
           #else                                             // - or -
             ACTION_ITEM(MSG_RELEASE_MEDIA, []{              // M22 Release Media
               queue.inject(F("M22"));
